f3386c2430edb9f47d620a48cc3574d187190a2d
thxnewlife <thxnewlife@gmail.com>
2017-10-20 15:40:48 +0900

    <endless>

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect("mongodb://cesco:c1203422!@ds125565.mlab.com:25565/cescomap");
+var db = mongoose.connection;
+db.once("open",function (){
+  console.log("DB conneted!");
+});
+db.on("error" ,function (err){
+  console.log("DB ERROR :" err)
+});
 
 app.set("view engine", 'ejs');
 app.use(express.static(path.join(__dirname + '/public')));
@@ -26,7 +36,7 @@ app.get('/', function (req,res){
   res.render('my_first_ejs');
 });
 
-var port = process.enc.PORT || 3000;
-app.listen(3000, function(){
+var port = process.enc.PORT || 5000;
+app.listen(5000, function(){
   console.log('Server On!');
 });
