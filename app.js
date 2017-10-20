4c462dd9b6f67cd19cf0ae3975e1e59068332988
thxnewlife <thxnewlife@gmail.com>
2017-10-20 14:22:32 +0900

    <static>

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
 
-app.get('/',function (req,res){
-  res.send('Cesco General Affairs Dept.');
+app.set("view engine", 'ejs');
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.get('/', function (req,res){
+  res.render('my_first_ejs');
 });
 
-app.listen(3000, function(){
+app.listen(5000, function(){
   console.log('Server On!');
 });
