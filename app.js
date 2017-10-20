041b27c7016f0b38966e2fa6bcd5892ec311b1ca
thxnewlife <thxnewlife@gmail.com>
2017-10-20 15:11:45 +0900

    <mola>

--- app.js
+++ app.js
@@ -5,10 +5,28 @@ var app = express();
 app.set("view engine", 'ejs');
 app.use(express.static(path.join(__dirname + '/public')));
 
+var data={count:0};
+app.get('/', function (req,res){
+  data.count++;
+  res.render('my_first_ejs',data);
+});
+app.get('/reset', function(req,res){
+  data.count=0;
+  res.render('my_first_ejs',data);
+});
+app.get('/set/count', function(req,res){
+  if(req.query.count) data.count=req.query.count;
+  res.render('my_first_ejs',data);
+});
+app.get('/set/:num', function (req,res){
+  data.count=req.params.num;
+  res.render('my_first_ejs',data);
+});
 app.get('/', function (req,res){
   res.render('my_first_ejs');
 });
 
-app.listen(5000, function(){
+var port = process.enc.PORT || 3000;
+app.listen(3000, function(){
   console.log('Server On!');
 });
